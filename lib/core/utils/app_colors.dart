251e81aeba68515928d54ea56d496b813a7990da
mesho2004 <progamer74316@gmail.com>
2025-11-27 00:33:43 +0200

    feat{watchlist screen}: built watchlist screen and created empty and full state

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -8,4 +8,5 @@ class AppColors {
   static const blueColor = Color(0xff0296E5);
   static const unselectedItemColor = Color(0xff67686D);
   static const ratingColor = Color(0xffFF8700);
+  static const subTextColor = Color(0xff92929D);
 }
